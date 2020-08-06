86bc3b6ac98c9844ec912aef11135a1d03cc7dd6
Anthony Shull <shull.anthony@gmail.com>
2020-08-06 16:41:11 -0500

    Diffs being sent out over ws

--- processor.rkt
+++ processor.rkt
@@ -4,14 +4,45 @@
 
 (provide make-processor)
 
+(define (tablify-helper table uids data)
+  (cond [(hash? data)
+	 (let ([keys (hash-keys data)])
+	   (if (empty? keys)
+	       '()
+	       (let ([key (first keys)])
+		 (append
+		  (tablify-helper table (append uids (list key)) (hash-ref data key))
+		  (tablify-helper table uids (hash-remove data key))))))]
+	[(integer? data)
+	 (cons (list (string-join (map symbol->string uids) ".") data) table)]
+	[else
+	 table]))
+
+(define (tablify data)
+  (tablify-helper '() '() data))
+
+(define (plus-minus a b)
+  (cond
+   [(= a b) "="]
+   [(> b a) "+"]
+   [(< b a) "-"]))
+
+(define (diff left-right)
+  (let ([olds (tablify (first left-right))]
+	[news (tablify (second left-right))])
+    (for/list ([old olds]
+	       [new news]
+	       #:when (equal? (first old) (first new)))
+      (list (hash-ref (hash-ref (first left-right) 'metadata) 'uid)
+	    (first old)
+	    (plus-minus (second old) (second new))
+	    (abs (- (second old) (second new)))))))
+
 (define (make-processor out)
   (thread
    (lambda ()
      (let loop ()
-       (sleep 0.5)
        (~>> (thread-receive)
-	    ;; process
-	    ;; send to next thread
-	    ;; ((lambda (s) (displayln s) s))
+	    (diff)
 	    (thread-send out))
        (loop)))))
